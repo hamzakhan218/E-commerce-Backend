212f79419b515370bdd6e7a26e2447831592037d
Hamza Bashir <hamzabashir218@gmail.com>
2023-08-22 18:58:21 +0500

    FIX 5

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { useContainer } from "class-validator";
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors({
-		origin: ["https://e-commerce-frontend-chi-inky.vercel.app/"],
-		methods: ["POST", "PUT", "DELETE", "GET", "OPTIONS"],
-	});
+
+	const corsOptions: CorsOptions = {
+		origin: "https://e-commerce-frontend-chi-inky.vercel.app", // Replace with your frontend's domain
+		methods: ["GET", "POST", "PUT", "DELETE"], // Allow the necessary HTTP methods
+		credentials: true, // If your app requires credentials (e.g., cookies)
+	};
+
+	app.enableCors(corsOptions);
+
 	app.useGlobalPipes(
 		new ValidationPipe({
 			whitelist: true,
