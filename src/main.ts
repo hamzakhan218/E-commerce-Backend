9c986687398b8700b06a366fd504c805b6722142
Hamza Bashir <hamzabashir218@gmail.com>
2023-08-22 19:11:22 +0500

    FiX 6

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,32 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { useContainer } from "class-validator";
 import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
+import { Request, Response, NextFunction } from "express";
+import { MiddlewareFunction } from "@nestjs/common/interfaces/middleware/middleware-function.interface";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	const corsOptions: CorsOptions = {
-		origin: "https://e-commerce-frontend-chi-inky.vercel.app", // Replace with your frontend's domain
+		origin: ["*", "https://e-commerce-frontend-chi-inky.vercel.app"], // Replace with your frontend's domain
 		methods: ["GET", "POST", "PUT", "DELETE"], // Allow the necessary HTTP methods
 		credentials: true, // If your app requires credentials (e.g., cookies)
 	};
 
 	app.enableCors(corsOptions);
+
+	// Middleware to handle preflight (OPTIONS) requests
+	const handlePreflight: MiddlewareFunction = (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) => {
+		res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+		res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
+		next();
+	};
+
+	app.use(handlePreflight);
 
 	app.useGlobalPipes(
 		new ValidationPipe({
